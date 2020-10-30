08d5740fc96d19f2b381594f0a5e037cebae4853
Alvaro Argañaraz <arganarazalvaro@gmail.com>
2020-10-29 22:54:55 -0300

    first commit

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,56 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Alvaro Argañaraz | Front End Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Alvaro Argañaraz',
+  subtitle: "I'm a Front End Developer",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: "Hi! I'm Alvaro Argañaraz, a Front End developer. I love learning new technologies and facing challenges to grow and improve.",
+  paragraphTwo: "My skills include HTML5, CSS3, Bootstrap 4, SASS, Javascript ES6+, React.js Hooks, Redux, Jquery, Git, and more.",
+  paragraphThree: "I studied engineering and management, which helped me gain soft skills and experience, which add to my skills as a developer. You can read more about my studies, skills, experiences and education in my resume below.",
+  resume: 'https://drive.google.com/file/d/1Q4IzLuiCLcI9SBjmTsMGTtzOm6YQc9U8/view', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'ReactMovies.JPG',
+    title: 'React Movies',
+    info: 'This is a Movie App that shows info about all movies!',
+    info2: 'It was made using React Hooks and Styled Components.',
+    url: 'https://awesome-react-movies.netlify.app/',
+    repo: 'https://github.com/arganarazalvaro/react-movies', // if no repo, the button will not show up
+  },
+    {
+    id: nanoid(),
+    img: 'Recipe.JPG',
+    title: 'Recipe Finder',
+    info: 'This is a web application that will help you find the best recipes! ',
+    info2: 'It was done using React and Edaman´s API.',
+    url: 'https://amazing-recipe.netlify.app/',
+    repo: 'https://github.com/arganarazalvaro/recipes-aplication', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'Consultora.JPG',
+    title: 'Argañaraz Consultores',
+    info: 'This is a website for a business consulting company. ',
+    info2: 'It was made with HTML5, CSS3 and JavaScript.',
+    url: 'https://arganarazalvaro.github.io/consultoraarganaraz/',
+    repo: 'https://github.com/arganarazalvaro/consultoraarganaraz', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +59,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'arganarazalvaro@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +67,23 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/alvaro-arganaraz/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/arganarazalvaro/',
+    },
+    {
+      id: nanoid(),
+      name: 'codepen',
+      url: 'https://codepen.io/AlvaroArganaraz',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
